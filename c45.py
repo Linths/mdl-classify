0c14d10f209bb99250994d36861814fea981327e
Lindsay Kempen <code@linths.com>
2020-04-19 14:36:15 +0200

    Show tree of C4.5

--- c45.py
+++ c45.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
+import sklearn.tree as sktree
 from sklearn import metrics
+import numpy as np
+import matplotlib.pyplot as plt
 
 FEATURE_COLS = ["cap-shape","cap-surface","cap-color","bruises","odor","gill-attachment","gill-spacing","gill-size","gill-color","stalk-shape","stalk-root","stalk-surface-above-ring","stalk-surface-below-ring","stalk-color-above-ring","stalk-color-below-ring","veil-type","veil-color","ring-number","ring-type","spore-print-color","population","habitat"]
 
@@ -19,10 +22,16 @@ def applyC45(data, ratio=4/5):
 
     clf = DecisionTreeClassifier(random_state=0)
     clf.fit(train_data, train_labels)
+    
+    plt.figure()
+    sktree.plot_tree(clf)
+    plt.show()
+
     predicted = clf.predict(test_data)
     print("Accuracy: ", metrics.accuracy_score(test_labels, predicted))
-    print(cross_val_score(clf, data, labels, cv=10))
-
+    accs = cross_val_score(clf, data, labels, cv=10, scoring='accuracy')
+    print(accs)
+    print(np.average(accs))
 
 if __name__ == "__main__":
     data = pd.read_csv('data/mushrooms.csv')
